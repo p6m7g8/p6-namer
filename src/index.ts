e97e09f7bbad4ad0cc540943d2ba52e5f8af4188
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-11-27 12:36:11 -0500

    fix(lambda): Revert "refactor(lambda): converts to aws-lambda-nodejs (#113)" (#114)
    This reverts commit 3397359a32dc0baf10d00f5cfa153d7d419cdc1a.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-export * from './p6namer';
+import * as path from 'path';
+
+import * as iam from '@aws-cdk/aws-iam';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as cdk from '@aws-cdk/core';
+import * as cr from '@aws-cdk/custom-resources';
+
+export interface IP6NamerProps {
+  accountAlias: string;
+}
+
+export class P6Namer extends cdk.Resource {
+  constructor(scope: cdk.Construct, id: string, props: IP6NamerProps) {
+    super(scope, id);
+
+    const policy = new iam.PolicyStatement({
+      actions: ['iam:CreateAccountAlias'],
+      resources: ['*'],
+      effect: iam.Effect.ALLOW,
+    });
+
+    const onEvent = new lambda.Function(this, 'P6Namer/Lambda', {
+      runtime: lambda.Runtime.PYTHON_3_8,
+      timeout: cdk.Duration.seconds(30),
+      tracing: lambda.Tracing.ACTIVE,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../resources/lambda')),
+    });
+
+    onEvent.addToRolePolicy(policy);
+
+    const provider = new cr.Provider(this, 'P6Namer/Provider', {
+      onEventHandler: onEvent,
+    });
+
+    new cdk.CustomResource(this, 'P6Namer/CR', {
+      serviceToken: provider.serviceToken,
+      properties: {
+        AccountAlias: props.accountAlias,
+      },
+    });
+  }
+}
