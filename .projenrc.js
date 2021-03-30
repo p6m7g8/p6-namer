823475b73c91d79a1d8270fd9463b08565b44269
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-03-30 10:06:56 -0400

    chore(deps): bumps projen, cdk, others to current (#385)

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const { AwsCdkConstructLibrary } = require('projen');
 const project = new AwsCdkConstructLibrary({
   authorAddress: 'pgollucci@p6m7g8.com',
   authorName: 'Philip M. Gollucci',
-  cdkVersion: '1.94.0',
+  cdkVersion: '1.95.1',
   name: 'p6-namer',
   repository: 'https://github.com/p6m7g8/p6-namer.git',
   description: 'Sets the AWS IAM Account Alias with a Custom Resource',
